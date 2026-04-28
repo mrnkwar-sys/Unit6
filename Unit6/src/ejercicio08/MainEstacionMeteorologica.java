72c0d0a6228b36375928c51521d67f906b4e8ac4
Rei ヽ( ´￢`)ﾉ <mr.nkwar@iesnervion.es>
2026-04-28 14:39:08 +0200

    ◝(ᵔᵕᵔ)◜

--- Unit6/src/ejercicio08/MainEstacionMeteorologica.java
+++ Unit6/src/ejercicio08/MainEstacionMeteorologica.java
@@ -21,29 +21,65 @@ public class MainEstacionMeteorologica {
 			//Temperatura minima registrada
 			double minTemperaturaDia;
 			
+			//Temperaturas máxima y mínima registrada en todo el achivo
+			double maxTemp = Integer.MIN_VALUE;
+			double minTemp = Integer.MAX_VALUE;
+			
 			//Fecha en la que se ha registrado la temperatura
 			LocalDate fecha = LocalDate.now();
 			
-			//Se muestra el menú con todas las opciones
-			mostrarMenu();
-			opcion = sc.nextInt();
-			sc.nextLine();
+			//Variable que definirá si el fichero continúa teniendo contenido o no
+			String linea;
 			
-			switch(opcion) {
-			case 1 -> {
-				System.out.println("Introduzca la temperatura máxima del día: ");
-				maxTemperaturaDia = sc.nextDouble();
+			do {
+				//Se muestra el menú con todas las opciones
+				mostrarMenu();
+				opcion = sc.nextInt();
+				sc.nextLine();
 				
-				System.out.println("Introduzca la temperatura mínima del día: ");
-				minTemperaturaDia = sc.nextDouble();
-				
-				in.write("Fecha: " + fecha + " | Temperatura máxima: " + maxTemperaturaDia + " | Temperatura mínima: " + minTemperaturaDia);
-			}
-			case 2 -> {
-				
-			}
-			}
+				switch(opcion) {
+				case 1 -> {
+					System.out.println("Introduzca la temperatura máxima del día: ");
+					maxTemperaturaDia = sc.nextDouble();
+					
+					System.out.println("Introduzca la temperatura mínima del día: ");
+					minTemperaturaDia = sc.nextDouble();
+					
+					if (maxTemperaturaDia > maxTemp) {
+						maxTemp = maxTemperaturaDia;
+					} 
+					
+					if (minTemperaturaDia < minTemp) {
+						minTemp = minTemperaturaDia;
+					}
+					
+					in.write("Fecha: " + fecha + " | Temperatura máxima: " + maxTemperaturaDia + " | Temperatura mínima: " + minTemperaturaDia);
+					
+					in.newLine();
+					in.flush();
+					
+					System.out.println("Registro guardado");
+				}
+				case 2 -> {
+					try {
+						BufferedReader on = new BufferedReader(new FileReader("C:\\Users\\mr.nkwar\\git\\Unit6\\Unit6\\src\\ejercicio08\\EstacionMeteorologica"));
+						
+						while ((linea = on.readLine()) != null) {
+							System.out.println(linea);
+						}
+						
+						System.out.println("Temperatura máxima registrada en el archivo: " + maxTemp);
+						System.out.println("Temperatura mínima registrada en el archivo:  " + minTemp);
+						
+						on.close();
+					} catch (IOException e) {
+						e.getMessage();
+					}
+				}
+				}
+			} while (opcion != 3);
 
+			in.close();
 		} catch (IOException e) {
 			e.getMessage();
 		}
